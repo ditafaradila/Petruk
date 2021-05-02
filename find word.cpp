2f84daae09f277324395f19b3554e2787b79de31
auraahsn <78643049+auraahsn@users.noreply.github.com>
2021-05-02 19:36:34 +0700

    Update find word.cpp

--- find word.cpp
+++ find word.cpp
@@ -1,22 +1,92 @@
 #include <iostream>
 #include <cstring>
-include iostream std;
-char words[15][15]={ {'t','g','b','w','w','i','n','t','e','r','w','s','e','s','n'},
-					            {'a','a','u','n','t','t','m','m','h','f','o','o','d','n','b'},
-					            {'j','l','w','c','q','l','d','z','m','p','m','v','d','m','r'},
-					            {'a','s','a','g','m','q','u','w','v','v','b','s','o','h','i'},
-					            {'b','w','p','l','o','t','a','n','a','d','t','p','g','n','c'},
-					            {'r','e','w','n','g','o','d','j','c','p','n','a','t','n','k'},
-					            {'e','e','o','t','w','o','s','b','q','h','a','r','r','s','a'},
-				            	{'a','z','c','g','e','s','w','e','w','n','a','k','n','p','b'},
-				            	{'d','i','n','n','e','r','q','o','d','l','w','d','c','a','r'},
-				            	{'o','n','o','p','k','w','m','p','a','r','k','t','z','c','c'},
-					            {'q','b','f','r','o','g','m','a','m','w','p','w','e','e','y'},
-				            	{'l','q','z','q','n','n','m','r','z','j','j','s','c','l','g'},
-					            {'m','o','s','g','z','c','z','e','t','d','b','o','o','t','o'},
-				            	{'p','d','c','r','z','m','s','n','g','r','d','n','r','p','z'},
-				            	{'o','h','n','k','z','w','a','t','e','r','j','g','t','r','a'}
-				            	};
+using namespace std;
+
+char words[15][15]   =	{ {'t','g','b','w','w','i','n','t','e','r','w','s','e','s','n'},
+			{'a','a','u','n','t','t','m','m','h','f','o','o','d','n','b'},
+			{'j','l','w','c','q','l','d','z','m','p','m','v','d','m','r'},
+			{'a','s','a','g','m','q','u','w','v','v','b','s','o','h','i'},
+			{'b','w','p','l','o','t','a','n','a','d','t','p','g','n','c'},
+			{'r','e','w','n','g','o','d','j','c','p','n','a','t','n','k'},
+			{'e','e','o','t','w','o','s','b','q','h','a','r','r','s','a'},
+			{'a','z','c','g','e','s','w','e','w','n','a','k','n','p','b'},
+			{'d','i','n','n','e','r','q','o','d','l','w','d','c','a','r'},
+			{'o','n','o','p','k','w','m','p','a','r','k','t','z','c','c'},
+			{'q','b','f','r','o','g','m','a','m','w','p','w','e','e','y'},
+			{'l','q','z','q','n','n','m','r','z','j','j','s','c','l','g'},
+			{'m','o','s','g','z','c','z','e','t','d','b','o','o','t','o'},
+			{'p','d','c','r','z','m','s','n','g','r','d','n','r','p','z'},
+			{'o','h','n','k','z','w','a','t','e','r','j','g','t','r','a'} };
+
+//Fungsi Mencari Kata
+int searchwords (char *(input))
+{
+	int p_kt, cek, ketemu;
+	ketemu = 0;
+	p_kt = strlen (input);
+	
+	for (int i = 0; i < 15; i++)
+	{
+		for (int j = 0; j < 15; j++)
+		{
+			if (input[0] == words[i][j])
+			{
+				//HORIZONTAL KANAN
+				for (int k = 0; k < p_kt; k++)
+				{
+					if (input[k] == words[i][j + k])
+					{
+						cek = k;
+					}
+					else
+					{
+						break;
+					}
+				}
+				if (cek == p_kt -1)
+				{
+					ketemu += 1;
+				}
+				else
+				{
+					ketemu += 0;
+				}
+				cek = 0;
+				
+				//HORIZONTAL KIRI
+				for (int k = 0; k < p_kt; k++)
+				{
+					if (input[k] == words[i][j-k])
+					{
+						cek = k;
+					}
+					else
+					{
+						break;
+					}
+				}
+				if (cek == p_kt -1)
+				{
+					ketemu += 1;
+				}
+				else
+				{
+					ketemu += 0;
+				}
+				cek = 0;										
+			}
+		}
+	}
+	
+	if (ketemu > 0)
+	{
+		cout << "Ada" << endl;
+ 	}
+ 	else
+ 	{
+ 		cout << "Tidak Ada" << endl;
+	}
+}
 
 int main (){
 	int n;
@@ -45,6 +115,6 @@ int main (){
 	//Output N Kata	
 	cout << "Hasil: " << endl;
 	for (int i=0; i<n; i++){
-		searchword(*(input+i));
+		searchwords (*(input+i));
 	}	
 }
